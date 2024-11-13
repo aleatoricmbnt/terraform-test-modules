b1348b42f0526c94f428d363af061d1212d2f952
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-11-13 13:55:43 +0200

    added another submodule with a long name

---
+++ modules/another-long-module-name-without-terraform-prefix-to-test-the-dropdown/main.tf
@@ -0,0 +1,66 @@
+resource "terraform_data" "this" {
+  input = var.input
+  triggers_replace = var.triggers_replace
+}
+
+variable "input" {
+  default = <<EOT
+CeeF2TzvB@burp0EorS914cbeWA9wn*=
+URpcah*JSo@3q13xZvp$ZmnBfp@okaTO
+rrAbvkQ!Gc$PWeDm9H&42@ZT@7#t!j#+
+ER@ekXud58%%b&f+3Ecj7yuYej469jAh
+%@NY$CTMSNo*YaK6mB%S#1&Ns$+QN#Qe
+Dsrj6O8NQosfwh!FP&2Bnvz8Yr6AD2@R
+Kht@jwWhO0VoC%@K8rQZ4!HY096ajx0$
+76n$jn5WNNuST!@$FEp8Zd@eapYMnHsx
+@#V9vDTNvKfhgzgZhO8+ukFERy*K+2u%
+G2B$2g1z#U1jst&gkVGeSm4wA6pJ%M%s
+TNnd3cPsCPAW9bM7rZDT3K76aZShuve8
+dqE8yyNaxv4vbkWoqb7e0#D0P@5!uAXo
+uR*fP0vwG+*1Uo37dFx#H6Bea86xAg6N
+RujKnROothsCg$gqo4YDJyyoF%mbgjT2
+9g!9PQpvRKAS9ABX&TTV+wNx!TBymEc3
+!$ONb&vYjXu0eMVCR=&DK*XMJ+rG&y=d
+$Kk1@HfkmsCx82NvBj&3Usja5@eX2Dpm
+!$$F0bvpc5BPeaH&B8e62$Xr54D*Y*%M
+oDK+oDT@KTXt=ACQkEEQJjnee0Ka0Xoy
+MY0MU!9prR=yODEE%v*$xpdGVMhobOeb
+aY5MZCKOX1nY!*AsXQTo5PRgYx%YAax&
+Nvo6JXR$*Ut0pa@mA9K=$REFxE$5ru+q
+r$JmZphE&ZqUk2sJeBVEufEVOeGUhO#t
+=y=WpWJBMVznrJ+Ds#c+OFvPPBBaqdxt
+H%pY9tcV$+@&*!ZbV6P%A6u4QVReR3hV
+KGJ+Rzt@R4w+9WC6=PS56Yo0R%Ug+1b&
+55WaSSXEu6&eKt@K09bNnHg2Xg%4gBpN
+yE++jgCwMj98QtZC$4TbHfA@Kp9#FVCX
+@PjSWcRZ=3DA@Bz&z*QvJwj%bDnuk4&S
+Qq0!!&M%Obq5NzEa3YGmmu2&MM4KwNF4
+t5+7zY0+hdXp!!0&wrykyse0mfYbgkTr
+=+9DEqaZMjr!PEhM=5YADMgn%Pw$563Q
+U+8BrfP9FZtqtCAo52Vhj&@C6Y2@Cs+h
+0Z7$&A9K1cf6uxAJk0RoK9kskZN*Muy#
+gSYfeNGu&VXuAvXRty7mQx64t&XFy&96
+0WXPQO%1x3OgKYa2sNpaXWdA$$frjYG*
+w6HdssxB3!Y5z@RYdwN!Na02%VwcKGND
+BZkQOdcZ*%3r4Tt&G6@GfB4=bc!!@Yuh
+ystX=Pb%y5wPm7%70x4Y7gDoerof17c5
+2a#od=j1JpA8*e1rasSe!ogxjz7R7SNJ
+EOT
+}
+
+variable "triggers_replace" {
+  default = <<EOT
+Early in his tyrannical rule, Vandire's agent sent him word of a unique sisterhood of warrior-women living on the little known Agri-world of San Leor. Hearing of their ascetic way of life, their utter dedication to the Emperor, and in particular their skills at arms, Vandire decided that he would make a formal visit to the secluded convent of the "Daughters of the Emperor."
+
+The first that the inhabitants of San Leor knew about the state visit by the man who was the de facto lord of the Imperium was when several thousand of his retainers marched into their simple towns, and demanded that they show the High Lord the utmost devotion, on pain of death. Images of these vast processions were recorded by the spy-lenses of hundreds of Servo-skulls, and disseminated across every world in the Imperium not cut off by the raging Warp Storms. The image of the High Lord Vandire being "welcomed" to San Leor became a staple of Ecclesiarchy propaganda.
+
+When at last the vast cavalcade reached the gates of the convent in which the Daughters of the Emperor resided, Vandire found them barred and closed. The voice of the gatekeeper informed the High Lord that the Daughters of the Emperor recognised only the authority of the God-Emperor Himself, and would open their gates to no man. Vandire's attendants cringed in anticipation of an explosion of rage, yet uncharacteristically, the High Lord remained calm.
+
+In a serene manner, Vandire told the gatekeeper that he would prove that he spoke with the authority of the Emperor Himself, if they would allow him to enter their convent with but a small bodyguard. The Daughters of the Emperor acceded to Vandire's request, and the High Lord and a small group of his retainers were led into the central courtyard. There, Vandire announced to the gathered sisters that he would prove that the Emperor's Grace was upon him, and that should he lie, he would be slain.
+
+Vandire brandished his Rosarius, and ordered one of his bodyguards to shoot him. The man was dumbfounded, and fearful of harming his master, but Vandire insisted. Forcing his trembling hand to stillness, the guard aimed his Laspistol straight at Vandire's heart and fired. Vandire was consumed in a blinding white light, and when the vision of the gathered witnesses had cleared, they saw that he stood unharmed before them. All in the courtyard went down upon their knees, and the Daughters of the Emperor proclaimed their fealty to High Lord Vandire.
+
+In truth, Vandire had tricked the sisters, gambling that they would never have seen a Rosarius. It was the energy field projected by the generator within the ornate medallion that had turned the guard's shot, and not the protection of the Emperor. Nonetheless, the Daughters of the Emperor promised themselves to serve their new master, and at his word changed the name of their order to the "Brides of the Emperor."
+
+The sisters became Vandire's personal bodyguard, and their skill at arms was enhanced by training in the finest weapons and armour the High Lord could provide. The Brides of the Emperor would stand at Vandire's side wherever he went, and they were both his bodyguards and his executioners. Word of their dedication spread across the worlds of the Imperium, and none who entered the High Lord's presence would so much as look at him askance under the silent, watchful gaze of the Brides of the Emperor.
+EOT
